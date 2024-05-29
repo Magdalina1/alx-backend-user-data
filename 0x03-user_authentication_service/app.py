dc4b70b80549cd099bb75c22cccf4b93a5d1d509
Magdalina1 <majdalol95@gmail.com>
2024-05-29 12:08:06 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,3 +24,35 @@ def index() -> str:
     {"message": "Bienvenue"}
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    Register new users
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """
+    Log in a user if the credentials provided are correct, and create a new
+    session for them.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": f"{email}", "message": "logged in"})
+    resp.set_cookie("session_id", session_id)
+    return resp
